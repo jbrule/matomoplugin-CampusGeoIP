45a1615a257b5cdea4f4b8270b5cbaf980ba152d
josh <josh@24hr.lan>
2022-11-23 17:57:05 -0600

    Included location SQL

---
+++ docs/location-test-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO matomo.matomo_campusgeoip_locations (name,city,campus,region,country,org,provider,latitude,longitude,ts_created,ts_last_edit) VALUES
+	 ('Main House','Baltimore','MH','MD','US','HomeNOC','NAT',39.254654,-76.71096,'2022-11-23 15:02:29','2022-11-23 15:02:29'),
+	 ('Tree House','Baltimore','TH','MD','US','HomeNOC','NAT',39.254654,-76.71096,'2022-11-23 15:04:53','2022-11-23 15:04:53'),
+	 ('Lake House','Baltimore','LH','MD','US','HomeNOC','NAT',39.427048,-76.88323,'2022-11-23 15:07:33','2022-11-23 15:07:33'),
+	 ('Home Office','Baltimore','HO','MD','US','HomeNOC','NAT',39.254654,-76.71096,'2022-11-23 17:07:15','0000-00-00 00:00:00');
